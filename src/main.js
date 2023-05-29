aff8f1dda9ceeef9ac1b83fd469c87098fd140f7
Wshine233 <731894784@qq.com>
2023-05-29 15:45:36 +0800

    去除Vuetify无效代码

--- src/main.js
+++ src/main.js
@@ -3,16 +3,5 @@ import IndexPage from "./App.vue"
 import {Button} from "vant";
 import "vant/lib/index.css";
 
-import 'vuetify/styles'
-import {createVuetify} from 'vuetify';
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-const vuetify = createVuetify({
-    components,
-    directives,
-})
-
 const app = createApp(IndexPage)
-app.use(Button).use(vuetify)
-app.mount("#app")
+app.use(Button).mount("#app")
